ba83dbef79ba140f0a5ff282366238f4f4e38d2b
Jorge Cisneros <fraxito@gmail.com>
2022-11-10 20:56:53 +0100

    Terminada la versión de clase (falta alguna cosa aviso)

--- src/main/java/org/dis/front/MyUI.java
+++ src/main/java/org/dis/front/MyUI.java
@@ -7,7 +7,12 @@ import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.*;
+import org.dis.back.BRException;
+import org.dis.back.EmpleadoBR;
+import org.dis.back.TipoEmpleado;
 import org.w3c.dom.Text;
+
+import java.util.Arrays;
 
 /**
  * This UI is the application entry point. A UI may either represent a browser window 
@@ -31,23 +36,59 @@ public class MyUI extends UI {
         final VerticalLayout layout = new VerticalLayout();
         final HorizontalLayout salarioBruto = new HorizontalLayout();
         final HorizontalLayout salarioNeto = new HorizontalLayout();
+        final VerticalLayout salarioBrutoContenedor = new VerticalLayout();
+        final VerticalLayout salarioNetoContenedor = new VerticalLayout();
 
-        TextField tipo = creaLabel("Tipo de empleado");
+        //TextField tipo = creaLabel("Tipo de empleado");
+
+        ComboBox <String> tipoEmpleadoComboBox = new ComboBox<>("Tipo de empleado", Arrays.asList(TipoEmpleado.ENCARGADO, TipoEmpleado.VENDEDOR));
         TextField ventasMes = creaLabel("Ventas del mes");
         TextField horasExtra = creaLabel("Nº de horas extra");
 
-        salarioBruto.addComponents(tipo,ventasMes,horasExtra);
+        TextField salarioBrutoLabel = creaLabel("Salario Bruto");
 
-        Button button = new Button("calcular Salario Bruto");
-        button.addClickListener(e -> {
+        salarioBruto.addComponents(tipoEmpleadoComboBox,ventasMes,horasExtra);
+        salarioNeto.addComponent(salarioBrutoLabel);
+        Button botonSalarioBruto = new Button("calcular Salario Bruto");
+        botonSalarioBruto.addClickListener(e -> {
+            String tipoEmpleadoIn = tipoEmpleadoComboBox.getValue();
+            double ventasMesIn = Double.parseDouble(ventasMes.getValue());
+            double horasExtrasIn = Double.parseDouble(horasExtra.getValue());
 
+            EmpleadoBR empleado = new EmpleadoBR();
+            try {
+                double resultado = empleado.calculaSalarioBruto(tipoEmpleadoIn, ventasMesIn, horasExtrasIn);
+                Label labelSalarioBruto = new Label("El salario bruto obtenido es "+ resultado + "€");
+                salarioBrutoContenedor.addComponent(labelSalarioBruto);
+            } catch (BRException ex) {
+                Label labelSalarioBruto = new Label(ex.getMessage());
+                salarioBrutoContenedor.addComponent(labelSalarioBruto);
+            }
         });
 
-        TabSheet tabs = new TabSheet();
-        tabs.addTab(salarioBruto).setCaption("Calcula Salario Bruto");
-        tabs.addTab(salarioNeto).setCaption("Calcula Salario Neto");
+        Button botonSalarioNeto = new Button("calcular Salario Neto");
+        botonSalarioNeto.addClickListener(e -> {
+            double salarioBrutoIn = Double.parseDouble(salarioBrutoLabel.getValue());
 
-        layout.addComponents(tabs, button);
+            EmpleadoBR empleado = new EmpleadoBR();
+            try {
+                double resultado = empleado.calculaSalarioNeto(salarioBrutoIn);
+                Label labelSalarioNeto = new Label("El salario Neto obtenido es "+ resultado + "€");
+                salarioNetoContenedor.addComponent(labelSalarioNeto);
+            } catch (BRException ex) {
+                Label labelSalarioNeto = new Label(ex.getMessage());
+                salarioNetoContenedor.addComponent(labelSalarioNeto);
+            }
+        });
+
+        salarioBrutoContenedor.addComponents(salarioBruto,botonSalarioBruto);
+        salarioNetoContenedor.addComponents(salarioNeto, botonSalarioNeto);
+
+        TabSheet tabs = new TabSheet();
+        tabs.addTab(salarioBrutoContenedor).setCaption("Calcula Salario Bruto");
+        tabs.addTab(salarioNetoContenedor).setCaption("Calcula Salario Neto");
+
+        layout.addComponent(tabs);
         
         setContent(layout);
     }
